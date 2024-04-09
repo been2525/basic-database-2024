359401810de57f7a004c4ab6f419dcda5238b141
been2525 <qkrtjdqls245@naver.com>
2024-04-09 16:48:32 +0900

    DB 9일차 종료

--- day09/코딩테스트_박성빈.sql
+++ day09/코딩테스트_박성빈.sql
@@ -15,33 +15,26 @@ SELECT [Names] AS '도서명'
  ORDER BY price DESC;
 
 -- 3번
-SELECT m.[Names] AS '회원명'
+ SELECT DISTINCT m.[Names] AS '회원명'
      , m.Levels AS '회원등급'
      , m.Addr AS '회원주소'
-     , r.rentalDate
-  FROM rentaltbl AS r
-     , membertbl AS m 
- WHERE m.memberIdx NOT IN (SELECT m.[Names] AS '회원명'
-                                                , m.Levels AS '회원등급'
-                                                , m.Addr AS '회원주소'
-                                                , r.rentalDate
-                                            FROM rentaltbl AS r
-                                                , membertbl AS m 
-                                            WHERE m.memberIdx = r.memberIdx)
- --ORDER BY 
+     , r.rentalDate AS '대여일'
+  FROM membertbl AS m LEFT OUTER JOIN rentaltbl AS r
+    ON m.memberIdx = r.memberIdx
+ WHERE r.rentalIdx IS NULL
+   AND m.Levels LIKE 'A';
 
 -- 4번
 SELECT d.[Names] AS '책 장르'
-     , FORMAT(SUM(b.Price), '#,#') AS '총합계금액'
+     , FORMAT(SUM(b.Price), '#,#') + '원' AS '총합계금액'
   FROM divtbl AS d, bookstbl As b
  WHERE d.Division = b.Division
  GROUP BY d.Division, d.[Names]
--- ORDER BY d.Division;
 
 -- 5번
 SELECT ISNULL(d.[Names], '--합계--') AS '책 장르'
-     , FORMAT(SUM(b.Price), '#,#') AS '총합계금액'
      , COUNT(b.Division) AS '권수'
+     , FORMAT(SUM(b.Price), '#,#') + '원' AS '총합계금액'
   FROM divtbl AS d, bookstbl As b
  WHERE d.Division = b.Division  
  GROUP BY d.Division, d.[Names] WITH ROLLUP
